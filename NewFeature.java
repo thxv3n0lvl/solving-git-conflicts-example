d4fa9243d3913d4713a90dc5662aba4e60810645
thxv3n0lvl <thxv3n0lvl@pm.me>
2021-10-23 16:39:28 +0200

    feature: adds a couple files and modify an old one.
    Con este commit se pretende agregar mas informacion al archivo
    que ya existe y agregar uno nuevo.

---
+++ NewFeature.java
@@ -0,0 +1,5 @@
+
+// Comentario aleatorio.
+public void main() {
+	System.out.println("Este es un nuevo commit, es la implementacion de 54969");	
+}
--- test.js
+++ test.js
@@ -1,4 +1,13 @@
 console.log("Este es el primer codigo del primer commit en master-main");
 
+// Generar codigo aqui
+var list = null
+new Promise(list);
+
 // Este va directo a main tambien
-console.log("Segunda edicion de codigo, para el segundo commit")
+console.log("UN CAMBIO EN LO QUE YA EXISTIA EN MAIN. Segunda edicion de codigo, para el segundo commit")
+
+
+// Otro bonche de codigo
+console.log("parte de 54969. Implementacion que genere conflicto en algun punto")
+
