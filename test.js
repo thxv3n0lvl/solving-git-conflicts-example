32843acf13301a24a2cd09e5be15b943d8ee6b1b
thxv3n0lvl <thxv3n0lvl@pm.me>
2021-10-23 16:56:51 +0200

    feature: adds more code to inflict a conflict

--- test.js
+++ test.js
@@ -1,4 +1,33 @@
+// Generar conflicto de comentario
 console.log("Este es el primer codigo del primer commit en master-main");
+
+
+let obj = {
+	name: "alan",
+	lastName: "trevino"
+	getName() {
+		return this.name;
+	}
+};
+
+console.log(obj);
 
 // Este va directo a main tambien
 console.log("Segunda edicion de codigo, para el segundo commit")
+
+/**
+ * Este es una funcion 
+ *
+ * @param bla bla
+ **/
+ function funcionAgregar() {
+ 	console.log(`
+ 		Aqui un mensaje un poco
+ 		mas largo
+ 		representando codigo
+ 		mas extenso.
+	`);
+ }
+
+
+ var listTwo = [];
